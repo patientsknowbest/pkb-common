49416a4968303d7bdcf3d417ce66c6f059fe9df5
Bence Erős <bence@patientsknowbest.com>
2018-04-30 13:08:34 +0200

    adding missing @FunctionalInterface annotation to ConfigLoader GDE-168

--- config/src/main/java/com/pkb/common/config/ConfigLoader.java
+++ config/src/main/java/com/pkb/common/config/ConfigLoader.java
@@ -2,6 +2,7 @@ package com.pkb.common.config;
 
 import java.util.function.Supplier;
 
+@FunctionalInterface
 interface ConfigLoader extends Supplier<RawConfigStorage> {
 
     RawConfigStorage load();
