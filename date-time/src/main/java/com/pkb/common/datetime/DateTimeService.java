f9e67d19d3e94d1d489dea3b5970878f04806fe1
Russell Bender <russell@patientsknowbest.com>
2018-07-31 00:54:59 +0100

    fix week range error for some days of the week (PHR-4028)

--- date-time/src/main/java/com/pkb/common/datetime/DateTimeService.java
+++ date-time/src/main/java/com/pkb/common/datetime/DateTimeService.java
@@ -2,14 +2,21 @@ package com.pkb.common.datetime;
 
 import java.time.Clock;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoField;
+
+import static java.time.ZoneOffset.UTC;
 
 public interface DateTimeService {
 
     default Instant now() {
         ZonedDateTime.ofInstant(clock().instant(), clock().getZone());
         return clock().instant();
+    }
+
+    default LocalDate today() {
+        return LocalDate.now(UTC);
     }
 
     Clock clock();
