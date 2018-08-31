3afa862b3e6fcba00c79833fd7873d19a25d6e37
Gabor Karsai <gabor@patientsknowbest.com>
2018-08-31 14:25:58 +0200

    Fixed timestamp reset

--- date-time/src/main/java/com/pkb/common/datetime/FakeDateTimeService.java
+++ date-time/src/main/java/com/pkb/common/datetime/FakeDateTimeService.java
@@ -1,9 +1,15 @@
 package com.pkb.common.datetime;
 
+import java.lang.invoke.MethodHandles;
 import java.time.Clock;
 import java.time.ZonedDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FakeDateTimeService implements DateTimeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private volatile Clock currentFixedClock;
 
@@ -13,7 +19,8 @@ public class FakeDateTimeService implements DateTimeService {
         this.fallbackService = fallbackService;
     }
 
-    @Override public Clock clock() {
+    @Override
+    public Clock clock() {
         if (currentFixedClock == null) {
             return fallbackService.clock();
         }
@@ -23,10 +30,12 @@ public class FakeDateTimeService implements DateTimeService {
     public void setCurrentTime(String input) {
         ZonedDateTime zdt = ZonedDateTime.parse(input);
         currentFixedClock = Clock.fixed(zdt.toInstant(), zdt.getZone());
+        LOGGER.info("Set fake date time to: {}", currentFixedClock);
     }
 
     public void forgetCurrentTime() {
         this.currentFixedClock = null;
+        LOGGER.info("Cleared fake date time.");
     }
 
 }
