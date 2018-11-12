9768485b27ef07a78359c3da82092cf61cd0ac87
Martin Ashby <martin@patientsknowbest.com>
2018-11-12 17:13:11 +0000

    Switched to an annotation on the test to enable detailed logging, so that the application doesn't have to know about what test is running. PHR-4543

--- testlogging/src/main/java/com/pkb/common/testlogging/DetailLoggingProvider.java
+++ testlogging/src/main/java/com/pkb/common/testlogging/DetailLoggingProvider.java
@@ -1,18 +1,19 @@
 package com.pkb.common.testlogging;
 
 import java.util.Collection;
-import java.util.function.Predicate;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-public interface TestLoggingService {
-    void setCurrentTest(String test);
-    void logForTest(Predicate<String> testNameMatcher, String log, Object... params);
+public interface DetailLoggingProvider {
+    Logger obtainLogger(Class<?> clazz);
 
     static <T> String collectionToDebugString(Collection<T> objects) {
         return objects.stream()
                 .map(ToStringBuilder::reflectionToString)
                 .collect(Collectors.joining(", "));
     }
+
+    void setDetailedLoggingRequired(boolean detailedLoggingRequired);
 }
