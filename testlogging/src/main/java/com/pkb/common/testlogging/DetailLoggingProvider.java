0fbdb237d236d7ec435a8a59a0a94d91c99fafed
Martin Ashby <martin@ashbysoft.com>
2018-11-13 21:18:36 +0000

    https://www.infoq.com/presentations/Null-References-The-Billion-Dollar-Mistake-Tony-Hoare PHR-4543

--- testlogging/src/main/java/com/pkb/common/testlogging/DetailLoggingProvider.java
+++ testlogging/src/main/java/com/pkb/common/testlogging/DetailLoggingProvider.java
@@ -5,14 +5,24 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface DetailLoggingProvider {
     Logger obtainLogger(Class<?> clazz);
 
-    static <T> String collectionToDebugString(Collection<T> objects) {
-        return objects.stream()
-                .map(ToStringBuilder::reflectionToString)
-                .collect(Collectors.joining(", "));
+    static <T> @NotNull String collectionToDebugString(@Nullable Collection<T> objects) {
+        if (objects == null) {
+            return "null";
+        } else {
+            return objects.stream()
+                    .map(ToStringBuilder::reflectionToString)
+                    .collect(Collectors.joining(", "));
+        }
+    }
+
+    static <T> @NotNull String nullSafeReflectionToString(@Nullable T object) {
+        return object == null ? "null" : ToStringBuilder.reflectionToString(object);
     }
 
     void setDetailedLoggingRequired(boolean detailedLoggingRequired);
