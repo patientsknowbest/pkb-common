6805920d4b182cc7cc23196deb1658717ab9e7d0
Steve Gilbert <steve@patientsknowbest.com>
2019-01-09 11:26:29 +0000

    Copied EncounterClass across from pkb-remote-client

--- enum/src/main/java/com/pkb/entities/enums/EncounterClass.java
+++ enum/src/main/java/com/pkb/entities/enums/EncounterClass.java
@@ -1,4 +1,9 @@
 package com.pkb.entities.enums;
 
-public class EncounterClass {
+// From http://hl7.org/fhir/v3/ActEncounterCode/vs.html
+
+public enum EncounterClass {
+
+    IN_PATIENT, OUT_PATIENT, AMBULATORY, EMERGENCY, HOME, FIELD, DAYTIME, VIRTUAL, OTHER
+
 }
