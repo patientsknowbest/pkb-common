dfb53a2ddcc5d185fe8bf115c07c5b3526d456f0
Steve Gilbert <steve@patientsknowbest.com>
2019-01-09 11:25:24 +0000

    Fixed link

--- enum/src/main/java/com/pkb/entities/enums/EncounterClass.java
+++ enum/src/main/java/com/pkb/entities/enums/EncounterClass.java
@@ -1,9 +1,4 @@
 package com.pkb.entities.enums;
 
-// Values taken from http://www.hl7.org/implement/standards/fhir/encounter-class.html
-
-public enum EncounterClass {
-
-    IN_PATIENT, OUT_PATIENT, AMBULATORY, EMERGENCY, HOME, FIELD, DAYTIME, VIRTUAL, OTHER
-
+public class EncounterClass {
 }
