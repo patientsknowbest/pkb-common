53e176984dbd462c384ea90daef251624efec261
Rob Whelan <rob@patientsknowbest.com>
2019-01-15 09:22:44 +0100

    cherry-picking ee1eb99b039795b168ad470395e6e69ae8c3b1e5 and resolving conflicts

--- date-time/src/main/java/com/pkb/common/datetime/DefaultDateTimeService.java
+++ date-time/src/main/java/com/pkb/common/datetime/DefaultDateTimeService.java
@@ -4,8 +4,18 @@ import java.time.Clock;
 
 public class DefaultDateTimeService implements DateTimeService {
 
-    @Override public Clock clock() {
+    @Override
+    public Clock clock() {
         return Clock.systemDefaultZone();
     }
 
+    @Override
+    public void setFixedCurrentTimeForTesting(String input) {
+        throw new IllegalStateException("Not currently in a test environment");
+    }
+
+    @Override
+    public void forgetFixedCurrentTimeForTesting() {
+        throw new IllegalStateException("Not currently in a test environment");
+    }
 }
