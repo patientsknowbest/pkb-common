ce7331df9a0ea06d1a010bcece746bb2d587a73e
Rob Whelan <rob@patientsknowbest.com>
2019-01-17 01:53:40 +0100

    pass required other services as arg AI-426 also clear out more unneeded logging

--- config/src/main/java/com/pkb/common/config/Configuration.java
+++ config/src/main/java/com/pkb/common/config/Configuration.java
@@ -8,6 +8,8 @@ public interface Configuration {
 
     int getMenudataQueryBatchSize();
 
+    boolean isFakeDateTimeServiceEnabled();
+
     boolean isFhirAppointmentResourceEnabled();
 
     boolean isFhirEncounterResourceEnabled();
