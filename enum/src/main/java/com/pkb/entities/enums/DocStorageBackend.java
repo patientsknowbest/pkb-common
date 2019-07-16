305d9f92d195b0601fc4b1f2651fa5092b8cd9e0
matevarga <m@matevarga.net>
2019-07-16 16:07:46 +0200

    Adds DocStorageBackend type and extends PublicUploadedData AI-490

---
+++ enum/src/main/java/com/pkb/entities/enums/DocStorageBackend.java
@@ -0,0 +1,12 @@
+package com.pkb.entities.enums;
+
+/**
+ * Matches PSQL enum type doc_storage_backend_type
+ */
+public enum DocStorageBackend {
+    DOC,
+    IN_MEMORY,
+    GCS,
+    FIRESTORE
+}
+
