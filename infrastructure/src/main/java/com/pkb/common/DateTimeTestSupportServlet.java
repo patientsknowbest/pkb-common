85603dec39e6c77e9cb366e9b6774adc1efc1ba6
Russ Gray <russgray+github@gmail.com>
2019-08-13 14:34:22 +0100

    Add DateTimeTestSupportServlet GDE-1409

---
+++ infrastructure/src/main/java/com/pkb/common/DateTimeTestSupportServlet.java
@@ -0,0 +1,35 @@
+package com.pkb.common;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.pkb.common.datetime.DateTimeService;
+
+public class DateTimeTestSupportServlet extends HttpServlet {
+
+    private DateTimeService dateTimeService;
+
+    @Inject
+    public DateTimeTestSupportServlet(DateTimeService dateTimeService) {
+        this.dateTimeService = dateTimeService;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println(dateTimeService.now());
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            String dateTime = req.getParameter("dateTime");
+            dateTimeService.setFixedCurrentTimeForTesting(dateTime);
+        } catch (IllegalStateException e) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+}
