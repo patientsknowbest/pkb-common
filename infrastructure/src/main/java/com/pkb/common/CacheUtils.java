ff804b7e9502d50b2e7fe3427111f4b76948f283
Steve Gilbert <steve@patientsknowbest.com>
2019-10-21 09:43:51 +0100

    GDE-1543 - pkb-common utility to check spring caches are clearable

---
+++ infrastructure/src/main/java/com/pkb/common/CacheUtils.java
@@ -0,0 +1,32 @@
+package com.pkb.common;
+
+import static java.util.stream.Collectors.toList;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import com.google.common.reflect.ClassPath;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+
+public class CacheUtils {
+
+    public static List<Class> getUnclearableCaches() throws Exception {
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        return  ClassPath.from(cl).getTopLevelClassesRecursive("com.pkb").stream()
+                .map(ClassPath.ClassInfo::load)
+                .filter(c -> hasCacheableMethods(c) && !ClearableInternalState.class.isAssignableFrom(c))
+                .collect(toList());
+    }
+
+    private static boolean hasCacheableMethods(Class clazz) {
+        for (Method m: clazz.getDeclaredMethods()) {
+            if (m.getAnnotation(Cacheable.class) != null || m.getAnnotation(CachePut.class) != null || m.getAnnotation(CacheEvict.class) != null) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
