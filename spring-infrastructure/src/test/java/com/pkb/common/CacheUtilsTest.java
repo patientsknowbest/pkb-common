f781742ef0663809a63cab593ec9ee22e1444e76
Steve Gilbert <steve@patientsknowbest.com>
2019-11-11 21:45:47 +0000

    GDE-1543 - tweaks after review

--- spring-infrastructure/src/test/java/com/pkb/common/CacheUtilsTest.java
+++ spring-infrastructure/src/test/java/com/pkb/common/CacheUtilsTest.java
@@ -2,29 +2,38 @@ package com.pkb.common;
 
 import static com.pkb.common.CacheUtils.getUnclearableCaches;
 
-import static java.util.Collections.singletonList;
-
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
-
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 
 import com.github.karsaig.approvalcrest.MatcherAssert;
 import com.github.karsaig.approvalcrest.matcher.Matchers;
 import com.pkb.testing.gson.DefaultPKBGsonConfigurationFactory;
 
-class NaughtyTestClass {
+class NaughtyTestClass1 {
     @Cacheable("naughtyCache")
     public String getStuff() {
         return "No";
     }
 }
 
+class NaughtyTestClass2 {
+    @CachePut("naughtyCache")
+    public void putStuff(String stuff) { }
+}
+
+class NaughtyTestClass3 {
+    @CacheEvict("naughtyCache")
+    public void evictStuff(String stuff) { }
+}
+
 class NiceTestClass implements ClearableInternalState {
     @Override
-    public void clearState() {
-    }
+    public void clearState() { }
 
     @Cacheable("niceCache")
     public String getStuff() {
@@ -43,7 +52,7 @@ public class CacheUtilsTest {
     @Test
     public void testGetUnclearableCaches() throws Exception {
         List<Class> actual = getUnclearableCaches();
-        List<Class> wanted = singletonList(NaughtyTestClass.class);
+        List<Class> wanted = Arrays.asList(NaughtyTestClass1.class,NaughtyTestClass2.class,NaughtyTestClass3.class);
         MatcherAssert
                 .assertThat(actual, Matchers.sameBeanAs(wanted).withGsonConfiguration(DefaultPKBGsonConfigurationFactory.pkbConfigWithVavr()));
     }
