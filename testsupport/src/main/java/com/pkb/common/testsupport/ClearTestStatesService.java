3819d8771100c44277ee906a8f1b9c2818fdd758
James Fox <james@patientsknowbest.com>
2020-08-10 19:29:01 +0200

    Introduce an abstract class to allow different clearStates for ReportRunner and KMS PHR-6960

--- testsupport/src/main/java/com/pkb/common/testsupport/ClearTestStatesService.java
+++ testsupport/src/main/java/com/pkb/common/testsupport/ClearTestStatesService.java
@@ -1,5 +1,7 @@
 package com.pkb.common.testsupport;
 
+import com.pkb.common.config.ConfigStorage;
+import com.pkb.common.datetime.DateTimeService;
 import com.pkb.pulsar.payload.ClearTestStatesRequest;
 import com.pkb.pulsar.payload.ClearTestStatesResponse;
 import org.slf4j.Logger;
@@ -9,15 +11,31 @@ public class ClearTestStatesService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(java.lang.invoke.MethodHandles.lookup().lookupClass());
 
-    public ClearTestStatesService() {
+    private final DateTimeService dateTimeService;
+    private final ConfigStorage configStorage;
+
+    public ClearTestStatesService(DateTimeService dateTimeService, ConfigStorage configStorage) {
+        this.dateTimeService = dateTimeService;
+        this.configStorage = configStorage;
     }
 
     public ClearTestStatesResponse process(ClearTestStatesRequest message) {
-        LOGGER.info("InjectConfigValueService.process message received");
+        LOGGER.info("ClearTestStatesService.process message received");
 
-        // TODO JAMES: do something here
+        boolean clearFixedTimestamp = message.getClearFixedTimestamp();
+        if (clearFixedTimestamp) {
+            dateTimeService.forgetFixedCurrentTimeForTesting();
+        }
 
-        LOGGER.info("InjectConfigValueService.process done.");
+        configStorage.reset();
+
+        // TODO: add a TestSupportAgent to the KMS so that we reset the state there
+        // RESTMessagingHelper.kmsDeleteCall(appGroup, "/kmstest/state", 204);
+
+        // TODO JAMES: add something to do the survey results in reportrunner
+        // given().delete(appGroup.reportRunnerUri() + "/survey_results").then().statusCode(204);
+
+        LOGGER.info("ClearTestStatesService.process done.");
 
         // TODO JAMES: set a response value
         return ClearTestStatesResponse.newBuilder().build();
