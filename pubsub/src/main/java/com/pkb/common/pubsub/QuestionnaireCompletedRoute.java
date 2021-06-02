400598cb07947eae426a6dabb0e29ab4606bb042
Balazs Bekesi <balazs@bekesi.com>
2021-06-02 10:22:49 +0200

    Rename consultation to questionnaire BI-225

--- pubsub/src/main/java/com/pkb/common/pubsub/QuestionnaireCompletedRoute.java
+++ pubsub/src/main/java/com/pkb/common/pubsub/QuestionnaireCompletedRoute.java
@@ -5,21 +5,21 @@ import org.apache.camel.Endpoint;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.builder.RouteBuilder;
 
-public abstract class ConsultationCompletedRoute extends RouteBuilder {
+public abstract class QuestionnaireCompletedRoute extends RouteBuilder {
 
-    @EndpointInject(property = "consultationEventTopicUri")
-    private Endpoint consultationEventTopic;
+    @EndpointInject(property = "questionnaireEventTopicUri")
+    private Endpoint questionnaireEventTopic;
 
     public abstract IGeneralPubsubConfig config();
 
     public String getConsultationEventTopicUri() {
-        return String.format("google-pubsub:%s:consultationEventTopic", config().getProject());
+        return String.format("google-pubsub:%s:questionnaireEventTopicUri", config().getProject());
     }
 
     @Override
     public void configure() {
-        from("direct:consultationEventTopic")
-                .to(consultationEventTopic);
+        from("direct:questionnaireEventTopicUri")
+                .to(questionnaireEventTopic);
     }
 
 }
