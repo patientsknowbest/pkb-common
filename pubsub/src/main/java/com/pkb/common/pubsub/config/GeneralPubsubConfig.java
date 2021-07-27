2a049736b376ceb31aad12739f15ac801fba1464
Balazs Bekesi <balazs@bekesi.com>
2021-07-27 18:40:28 +0200

    Add serviceaccountkey to route uri

--- pubsub/src/main/java/com/pkb/common/pubsub/config/GeneralPubsubConfig.java
+++ pubsub/src/main/java/com/pkb/common/pubsub/config/GeneralPubsubConfig.java
@@ -3,14 +3,22 @@ package com.pkb.common.pubsub.config;
 public class GeneralPubsubConfig implements IGeneralPubsubConfig {
 
     private final String project;
+    private final String serviceAccountKey;
 
-    public GeneralPubsubConfig(String project) {
+    public GeneralPubsubConfig(String project, String serviceAccountKey) {
         this.project = project;
+        this.serviceAccountKey = serviceAccountKey;
     }
 
     @Override
     public String getProject() {
         return project;
     }
+
+    @Override
+    public String getServiceAccountKey() {
+        return serviceAccountKey;
+    }
+
 
 }
