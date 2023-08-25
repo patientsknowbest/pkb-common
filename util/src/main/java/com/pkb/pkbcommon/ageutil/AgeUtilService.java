03d702f49be732269d7c17262e0bf3c837522f54
James Fox <james@patientsknowbest.com>
2023-08-25 15:51:37 +0200

    Add new AgeUtilService PHR-11742

---
+++ util/src/main/java/com/pkb/pkbcommon/ageutil/AgeUtilService.java
@@ -0,0 +1,25 @@
+package com.pkb.pkbcommon.ageutil;
+
+import com.pkb.common.datetime.DateTimeService;
+
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+
+public class AgeUtilService {
+
+    private final DateTimeService dateTimeService;
+
+    public AgeUtilService(DateTimeService dateTimeService) {
+        this.dateTimeService = dateTimeService;
+    }
+
+    public final static int MINIMUM_AGE_LETTER_INVITATION = 16;
+
+    public boolean patientIsOlderThan(LocalDate dateOfBirth, int ageInYears) {
+        return dateOfBirth.plus(ageInYears, ChronoUnit.YEARS)
+                .atStartOfDay(ZoneOffset.UTC)
+                .toInstant()
+                .isBefore(dateTimeService.now());
+    }
+}
